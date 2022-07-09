ee528d9d6e63ed1481bc83caf3302fa2bb1d6c74
krita <108590406+0xkrita@users.noreply.github.com>
2022-07-08 23:13:21 -0700

    feat: add search box and hook up with graphql

---
+++ components/Search.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState, FormEvent } from 'react';
+import { Preview } from './Preview';
+import { Suggestions } from './Suggestions';
+import { ZDK } from '@zoralabs/zdk';
+import { Chain, Network } from '@zoralabs/zdk/dist/queries/queries-sdk';
+
+const zdk = new ZDK({
+  endpoint: 'https://api.zora.co/graphql', 
+  networks: [
+    {
+      chain: Chain.Mainnet,
+      network: Network.Ethereum,
+    },
+  ]}
+);
+
+export interface ZNFT {
+  tokenId: string;
+  name: string;
+  description: string;
+  collectionAddress: string;
+  entityType: string;
+}
+
+export interface SearchQueryResponse {
+  search: {
+    nodes: ZNFT[];
+  };
+}
+
+export const Search = () => {
+  const [searchField, setSearchField] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showPreview, setShowPreview] = useState(false);
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      setSearchQuery(searchField);
+    }, 1000);
+    return () => clearTimeout(timeOutId);
+  }, [searchField])
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setShowPreview(true);
+    zdk.search({
+      query: searchField, 
+      pagination: { limit: 5 }
+    }).then(res => {
+      console.log(res.search.nodes[0])
+    })
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            name="name"
+            placeholder="search znft"
+            value={searchField}
+            onChange={(e) => {
+              setSearchField(e.target.value);
+              setShowPreview(false);
+            }}
+          />
+        </label>
+      </form>
+      <Suggestions text={searchQuery}></Suggestions>
+      { showPreview ? <Preview></Preview> : ''}
+    </>
+  );
+};
---
+++ components/Suggestions.tsx
@@ -0,0 +1,37 @@
+import { useQuery } from 'urql';
+import { SearchQueryResponse } from './Search';
+
+const SearchQuery = `
+  query ($text: String!) {
+    search(query: { text: $text }, pagination: { limit: 5 }) {
+      nodes {
+        tokenId
+        name
+        description
+        collectionAddress
+        entityType
+      }
+    }
+  }
+`;
+
+export const Suggestions = ({ text }: { text: string }) => {
+  const [{ data, fetching, error }] = useQuery<SearchQueryResponse>({
+    query: SearchQuery,
+    variables: { text },
+    pause: !text,
+  });
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  return (
+    <>
+    <ul>
+      {data?.search.nodes.map((znft) => (
+        <li key={znft.tokenId}>{JSON.stringify(znft, null, 2)}</li>
+      ))}
+    </ul>
+    </>
+  );
+};
