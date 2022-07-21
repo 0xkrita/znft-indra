7b8ee5d309eb504e5cfa3526f0bb5e253c49d258
krita <108590406+0xkrita@users.noreply.github.com>
2022-07-20 22:50:19 -0700

    feat: add small loading animation

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,1 +1,20 @@
-export const Loading = () => <p>Loading...</p>;
+import { animated, useSpring } from 'react-spring';
+
+export const Loading = () => {
+  const styles = useSpring({
+    loop: true,
+    from: { opacity: 1, diameter: 0 },
+    to: { opacity: 0, diameter: 100 },
+    delay: 400,
+  });
+
+  return (
+    <div className="flex text-xl">
+      Loading...
+      <animated.div
+        className="w-6 h-6 bg-emerald-600 rounded-full"
+        style={styles}
+      />
+    </div>
+  );
+};
